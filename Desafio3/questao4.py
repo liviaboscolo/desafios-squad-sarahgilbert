a7bb666abe653ab1e9c4bfb25258f59426ff3328
Nadi Duno <nadiduno@gmail.com>
2024-07-31 11:27:33 -0300

    Create questao4.py

---
+++ Desafio3/questao4.py
@@ -0,0 +1,14 @@
+contatos = {
+    "Leticia": "11 1234586789",
+    "Livia": "22 1234586789",
+    "Michelle": "33 1234586789",
+    "Nadi": "44 1234586789",
+    "Nicola":"55 1234586789",
+    "Raquel":"66 1234586789",
+    "Rosana": "77 1234586789"
+}
+nome = input("Digite o nome do contato: ")
+if nome in contatos:
+    print(f"Telefone de {nome} é {contatos[nome]}")
+else:
+    print("Contato não encontrado no registro.")
