903a6718d52a3a1ec190683e5997bc688ea0b3f7
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-08-04 11:12:28 -0300

    Questoes 5  do Desafio 1, 2, 3, 4

---
+++ Desafio3/questao5.py
@@ -0,0 +1,10 @@
+# Crie duas tuplas. Concatene-as para formar uma nova tupla.
+
+tupla1 = (1, 2, 3)
+tupla2 = (4, 5, 6)
+
+tupla_concatenada = tupla1 + tupla2
+
+print("Tupla 1:", tupla1)
+print("Tupla 2:", tupla2)
+print("Tupla concatenada:", tupla_concatenada)
---
+++ Desafio4/questao5.py
@@ -0,0 +1,22 @@
+#   5. Crie uma função chamada contar_vogais que recebe uma string
+# como parâmetro. Implemente a lógica para contar o número de vogais
+# na string e retorne o total de vogais. Solicite ao usuário para inserir uma
+# frase e utilize a função para contar as vogais.
+
+def contar_vogais(frase):
+    vogais = "aeiouAEIOU"
+    contador = 0
+    
+    for char in frase:
+        if char in vogais:
+            contador += 1
+            
+    return contador
+
+def main():
+    frase = input("Digite uma frase: ")
+    total_vogais = contar_vogais(frase)
+    print(f"O total de vogais na frase é: {total_vogais}")
+
+if __name__ == "__main__":
+    main()
